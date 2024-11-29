5c011c7db6192dc5c1cfb318caaab2fd496ee0a4
soulcarus <joaoicaromoreira@gmail.com>
2024-11-29 05:17:31 -0300

    refactor da fabrica de coleção

--- src/factories/collector_factory.py
+++ src/factories/collector_factory.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
-from clients.opc_ua_client import OPCUAClient
-from clients.mqtt_client import MQTTClient
-from strategies.mqtt_strategy import MQTTClient, MQTTStrategy
-from strategies.opcua_strategy import OPCUAClient, OPCUAStrategy
+from clients.apodi_client import APODIClient
+from clients.benatextil_client import BenatextilClient
+from strategies.benatextil_strategy import BenatextilMqttStrategy
+from strategies.apodi_strategy import ApodiOpcuaStrategy
 from typing import Dict, Any
 
-class CollectorFactory(ABC):
+class IndustryFactory(ABC):
     """Abstract factory for creating collector components."""
     
     @abstractmethod
@@ -18,34 +18,34 @@ class CollectorFactory(ABC):
         """Create a collection strategy instance."""
         pass
 
-class OPCUACollectorFactory(CollectorFactory):
-    """Factory for creating OPC UA collector components."""
+class ApodiOpcuaFactory(IndustryFactory):
+    """Factory for creating Apodi OPC UA collector components."""
 
     def __init__(self, industry_id: str, config: Dict[str, Any]):
         self.industry_id = industry_id
         self.config = config
 
-    def create_client(self) -> OPCUAClient:
-        """Create an OPC UA client instance."""
-        return OPCUAClient(self.config['server_link'])
+    def create_client(self) -> APODIClient:
+        """Create an Apodi OPC UA client instance."""
+        return APODIClient(self.config['server_link'])
 
-    def create_strategy(self) -> OPCUAStrategy:
-        """Create an OPC UA collection strategy instance."""
+    def create_strategy(self) -> ApodiOpcuaStrategy:
+        """Create an Apodi OPC UA collection strategy instance."""
         client = self.create_client()
-        return OPCUAStrategy(client, self.industry_id, self.config)
+        return ApodiOpcuaStrategy(client, self.industry_id, self.config)
 
-class MQTTCollectorFactory(CollectorFactory):
-    """Factory for creating MQTT collector components."""
+class BenatextilMQTTFactory(IndustryFactory):
+    """Factory for creating Benatextil MQTT collector components."""
 
     def __init__(self, industry_id: str, config: Dict[str, Any]):
         self.industry_id = industry_id
         self.config = config
 
-    def create_client(self) -> MQTTClient:
-        """Create an MQTT client instance."""
-        return MQTTClient(self.config['broker'], self.config.get('port', 1883))
+    def create_client(self) -> BenatextilClient:
+        """Create an Benatextil MQTT client instance."""
+        return BenatextilClient(self.config['broker'], self.config.get('port', 1883))
 
-    def create_strategy(self) -> MQTTStrategy:
-        """Create an MQTT collection strategy instance."""
+    def create_strategy(self) -> BenatextilMqttStrategy:
+        """Create an Benatextil MQTT collection strategy instance."""
         client = self.create_client()
-        return MQTTStrategy(client, self.industry_id, self.config)
+        return BenatextilMqttStrategy(client, self.industry_id, self.config)
